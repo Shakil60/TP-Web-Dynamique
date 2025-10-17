fad77e6e172d63448c4f37c9138e1eb20686e6da
Shakil <khaldishakil@gmail.com>
2025-10-17 09:24:54 +0200

    first commit

---
+++ src/main.go
@@ -0,0 +1,26 @@
+package main 
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func main() {
+	listTemplate, errTemp := template.ParseGlob("./templates/*.html")
+	if errTemp != nil {
+		fmt.Println(errTemp.Error())
+		os.Exit(1)
+	}
+
+	rootDoc, _ := os.Getwd()
+	fileserver := http.FileServer(http.Dir(rootDoc + "/assets"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
+
+	http.HandleFunc("/temp/index", func(w http.ResponseWriter, r *http.Request) {
+		listTemplate.ExecuteTemplate(w, "listproduits", nil)
+	})
+
+	http.ListenAndServe("localhost:8000", nil)
+}
